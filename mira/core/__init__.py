7dcd2c3257541b75d57d149eaee146c9f1ff2d22
Fausto Morales <fausto@robinbay.com>
2021-06-28 11:21:40 -0500

    Add a box cropping augmentation.

--- mira/core/__init__.py
+++ mira/core/__init__.py
@@ -1,4 +1,5 @@
-from .annotation import Annotation, AnnotationConfiguration  # noqa: F401
-from .scene import Scene, SceneCollection  # noqa: F401
-from .selection import Selection  # noqa: F401
-from . import utils  # noqa: F401
+from .annotation import Annotation, AnnotationConfiguration
+from .scene import Scene, SceneCollection
+from .selection import Selection
+from . import augmentations
+from . import utils
