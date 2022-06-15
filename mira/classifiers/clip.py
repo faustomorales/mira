0a76421d417e1e6ada31ea119788d15d68fd6c91
Fausto Morales <fausto@robinbay.com>
2022-06-15 16:14:26 -0500

    Add CLIP wrapper.

---
+++ mira/classifiers/clip.py
@@ -0,0 +1,87 @@
+import typing
+
+import typing_extensions as tx
+import mira.core
+import mira.thirdparty.clip.clip as clip
+
+import torch
+import torchvision
+import numpy as np
+
+ClassifierPrediction = tx.TypedDict(
+    "ClassifierPrediction", {"score": float, "logit": float, "label": str}
+)
+
+
+class CLIP:
+    """A wrapper for OpenAI's CLIP classifier."""
+
+    def __init__(self, annotation_config, model_name="RN50", device="cpu"):
+        self.model = clip.load(model_name, device=device)[0]
+        px = self.model.visual.input_resolution
+        self.preprocess = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize(
+                    px, interpolation=torchvision.transforms.InterpolationMode.BICUBIC
+                ),
+                torchvision.transforms.CenterCrop(px),
+                torchvision.transforms.Normalize(
+                    (0.48145466, 0.4578275, 0.40821073),
+                    (0.26862954, 0.26130258, 0.27577711),
+                ),
+            ]
+        )
+        self.device = device
+        self.model_name = model_name
+        self.annotation_config = annotation_config
+        self.text_vectors = self.model.encode_text(
+            clip.tokenize([c.name for c in self.annotation_config]).to(self.device)
+        )
+
+    def classify(
+        self, images: typing.List[typing.Union[str, np.ndarray]], batch_size=32
+    ) -> typing.List[ClassifierPrediction]:
+        """Classify a batch of images."""
+        predictions: typing.List[ClassifierPrediction] = []
+        for start in range(0, len(images), batch_size):
+            with torch.no_grad():
+                image_vectors = self.model.encode_image(
+                    torch.stack(
+                        [
+                            self.preprocess(
+                                torch.Tensor(
+                                    (
+                                        image
+                                        if isinstance(image, np.ndarray)
+                                        else mira.core.utils.read(image)
+                                    ).transpose(2, 0, 1)
+                                    / 255.0
+                                ).to(self.device)
+                            )
+                            for image in images[start : start + batch_size]
+                        ]
+                    )
+                )
+                logits = (
+                    self.model.logit_scale.exp()
+                    * (image_vectors / image_vectors.norm(dim=1, keepdim=True))
+                    @ (
+                        self.text_vectors / self.text_vectors.norm(dim=1, keepdim=True)
+                    ).t()
+                )
+                scores = logits.softmax(dim=-1).detach().cpu().numpy()
+                predictions.extend(
+                    [
+                        {
+                            "label": self.annotation_config[classIdx].name,
+                            "score": score,
+                            "logit": logit,
+                        }
+                        for score, classIdx, logit in zip(
+                            scores.max(axis=1),
+                            scores.argmax(axis=1),
+                            logits.detach().cpu().numpy().max(axis=1),
+                        )
+                    ]
+                )
+        return predictions
---
+++ mira/datasets/pixabay.py
@@ -0,0 +1,36 @@
+import os
+import math
+import urllib.parse
+import tqdm
+import requests
+import pandas as pd
+
+
+def download_dataset(query, apiKey, images_dir, maxImages=50, force=False):
+    """Builds a dataset and returns a dataframe index."""
+    assert maxImages <= 500, "Only 500 images can be downloaded."
+    os.makedirs(images_dir, exist_ok=True)
+    pageSize = min(200, maxImages)
+    sanitized = urllib.parse.quote(query, safe="")
+    base = "https://pixabay.com/api/?key={apiKey}&q={query}&per_page={pageSize}&page={page}"
+    first = requests.get(
+        base.format(apiKey=apiKey, query=sanitized, pageSize=pageSize, page=1)
+    ).json()
+    items = first["hits"]
+    pages = math.ceil(min(first["totalHits"], maxImages) / pageSize)
+    for page in range(2, pages + 1):
+        items.extend(
+            requests.get(
+                base.format(
+                    apiKey=apiKey, query=sanitized, pageSize=pageSize, page=page
+                )
+            ).json()["hits"]
+        )
+    for item in tqdm.tqdm(items):
+        url = item["webformatURL"]
+        filename = url.split("/")[-1]
+        item["filepath"] = os.path.join(images_dir, filename)
+        if not os.path.isfile(item["filepath"]) or force:
+            with open(item["filepath"], "wb") as f:
+                f.write(requests.get(url, allow_redirects=True).content)
+    return pd.DataFrame(items)
